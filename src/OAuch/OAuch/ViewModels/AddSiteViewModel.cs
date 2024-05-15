faff4fb5b2001e30514556c67bf68ddbbee9a87f
Pieter Philippaerts <pp@pieterp.be>
2024-05-15 12:14:49 +0200

    small update

--- src/OAuch/OAuch/ViewModels/AddSiteViewModel.cs
+++ src/OAuch/OAuch/ViewModels/AddSiteViewModel.cs
@@ -10,6 +10,12 @@ namespace OAuch.ViewModels {
         public string? Name { get; set; }
         [DisplayName("Metadata URL")]
         public string? MetadataUrl { get; set; }
+        [DisplayName("Metadata URL")]
+        public string? InitialDocuments { get; set; }
+        [DisplayName("Metadata URL")]
+        public Dictionary<string, string>? InitialDocumentTypes { get; set; }
+
+
 
         public IList<Site>? Sites { get; set; }
         public Site? ActiveSite { get; set; }
